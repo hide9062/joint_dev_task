faa67bfe915fa851c25c3dcb9d06ded12218966c
hede9062 <hide@hidenoMacBook-Air.local>
2020-03-07 19:11:08 +0900

    質問1.3.4の修正

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-p names.push("斎藤")
+p names. << ("斎藤")
 end
 
 def q2
@@ -20,15 +20,14 @@ def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-p  numbers.count{ |num| num == 3}
+p  numbers.count(3)
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-sports.delete(nil)
-p sports
+p newsports = sports.compact
 end
 
 def q5
