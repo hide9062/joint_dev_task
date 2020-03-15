f43e4055445b88ba1dead0dd23b9789439a8595d
hede9062 <hide@hidenoMacBook-Air.local>
2020-03-15 17:22:15 +0900

    質問11.12に回答

--- task.rb
+++ task.rb
@@ -64,6 +64,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names.each_with_index do |item,i|
+ puts "会員No.#{i+1} #{item}さん"
+ end
 
 end
 
@@ -82,6 +85,9 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  sports.flatten.uniq.each.with_index(1) do |sport,i|
+    puts "No#{i} #{sport}"
+  end
 
 end
 
@@ -89,7 +95,7 @@ def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+p sprintf "satou"
 end
 
 def q13
