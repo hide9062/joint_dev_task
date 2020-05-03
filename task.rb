6a9f89532c0c36acd5443fa7df0d54f2259b98a6
hede9062 <hide@hidenoMacBook-Air.local>
2020-05-03 10:07:11 +0900

    19の修正,20の回答

--- task.rb
+++ task.rb
@@ -143,12 +143,11 @@ end
 
 class Item
   # 以下を修正して下さい
-  def initialize(name:)
-    @name = name
+  attr_reader :name
+  def initialize(**params)
+      @name = params[:name]
   end
-  def name
-    @name
-  end
+
 end
 def q19
   # ここは変更しないで下さい
@@ -158,12 +157,38 @@ end
 
 class UserQ20
   # 以下に回答を記載
+attr_reader :name, :age
 
+def initialize(**params)
+  @name = params[:name]
+  @age = params[:age]
+end
 end
 
 class Zoo
   # 以下に回答を記載
+  def initialize(**params)
+    @name = params[:name]
+    @infant = params[:entry_fee][:infant]
+    @children = params[:entry_fee][:children]
+    @adult = params[:entry_fee][:adult]
+    @senior = params[:entry_fee][:senior]
+end
 
+def info_entry_fee(user)
+  fee = case user.age
+  when 0..5
+    entry_fee = @infant
+  when 6..12
+    entry_fee = @children
+  when 13..64
+    entry_fee = @adult
+  when 65..120
+    entry_fee = @senior
+  end
+  puts "#{user.name}さんの入場料金は#{fee}円です"
+
+end
 end
 
 
